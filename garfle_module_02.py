f45f95b7da31100a48e4f617ff8ea7a5f7e409d1
Nic Feliccia <nic@secretsmokestack.com>
2019-09-16 06:50:34 -0400

    getting ready to wrok on the reader.

--- garfle_module_02.py
+++ garfle_module_02.py
@@ -1,5 +1,10 @@
-
 from nltk import word_tokenize, ngrams
+
+import gnarfle_module_bst as bst
+
+
+connection_with_remulak = bst.start_a_sql_alchemy_session()
+
 
 
 # print(f'Finished search for job description textat {(time.time() - program_start) * 1000} ')
@@ -12,9 +17,8 @@ for phrase_length in range(1, 4):
     job_text_ngrams.extend(list(ngrams(job_text_tokenize, phrase_length)))
 print(f'Ended ngramming {(time.time() - program_start) * 1000} ')
 print(f'started making job string list {(time.time() - program_start) * 1000} ')
-job_text_string_list = [' '.join(word_tuple) for word_tuple in job_text_ngrams ]
+job_text_string_list = [' '.join(word_tuple) for word_tuple in job_text_ngrams]
 print(f'finsihed making job string list {(time.time() - program_start) * 1000} ')
 print(f'Started  counting  {(time.time() - program_start) * 1000} ')
-word_dictionary=Counter(job_text_string_list)
+word_dictionary = Counter(job_text_string_list)
 print(f'Finished Counting   {(time.time() - program_start) * 1000} ')
-
