abad987c6bedb8d3d766c34ed833f64013087cfe
Nic Feliccia <nic@secretsmokestack.com>
2019-09-25 17:38:42 -0400

    scrambler works perfect

--- scramble_improve.py
+++ scramble_improve.py
@@ -1,9 +1,7 @@
 import re
 from collections import namedtuple
 from itertools import permutations
-from random import randrange
 
-import pandas as pd
 from sqlalchemy import Integer, String, Date, Float, Column
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -36,8 +34,8 @@ def start_a_sql_alchemy_session():
     # initiate the database_session
     db_string = create_pg_login_string()
     db_six_cyl_engine = create_engine(db_string, echo=False)
-    database_session = sessionmaker()
-    return database_session(bind=db_six_cyl_engine)
+    DatabaseSession = sessionmaker()
+    return DatabaseSession(bind=db_six_cyl_engine)
 
 
 def super_clean_a_string(in_string: str):
@@ -92,30 +90,6 @@ while keep_getting_phrases:
         scramble_split = scramble_phrase.split()
         for i in range(1, len(scramble_split) + 1):
             scramble_permutations = permutations(scramble_split[:i])
-            scramble_phrase_list.extend([' '.join(group[0:3]) for group in scramble_permutations])
+            scramble_phrase_list.extend([' '.join(group) for group in scramble_permutations])
 
-scramble_phrase_set = set(scramble_phrase_list)
 
-for scramble_phrase in scramble_phrase_set:
-    scramble_words_sql = f'%{scramble_phrase}%'
-    scramble_list_words_query = session_with_remulak.query(SQLIndeedSearchResults.job_title).filter(
-        SQLIndeedSearchResults.job_title.ilike(scramble_words_sql))
-    scramble_list_words_query_count = scramble_list_words_query.count()
-    scramble_list_body_query = session_with_remulak.query(SQLIndeedSearchResults.job_text_raw).filter(
-        SQLIndeedSearchResults.job_text_raw.ilike(scramble_words_sql))
-    scramble_list_body_query_count = scramble_list_body_query.count()
-    ThisResult = ScrambleResult(scramble_phrase=f'{scramble_phrase}',
-                                ocurances_in_title=scramble_list_words_query_count,
-                                ocurances_in_text=scramble_list_body_query_count)
-    print(
-        f'Scramble Phrase {ThisResult.scramble_phrase} \t "Title Occurances" {getattr(ThisResult, "ocurances_in_title")} \t Text Occurances {ThisResult[2]}')
-    scramble_results_tuple_list.append(ThisResult)
-
-scramble_df = pd.DataFrame(scramble_results_tuple_list)
-scramble_df.sort_values(by=['ocurances_in_title', 'ocurances_in_text','scramble_phrase'], ascending=False, inplace=True)
-scramble_df.drop_duplicates(inplace=True)
-print('DataFrame', scramble_df)
-comparison_filename = f'permutations_compared-{randrange(1, 100)}.xlsx'
-with pd.ExcelWriter(f'.\output\{comparison_filename}', engine='xlsxwriter', mode='a+') as the_comparison_file:
-    scramble_df.to_excel(the_comparison_file, sheet_name=comparison_filename[0:30], index=False)
-session_with_remulak.close()
