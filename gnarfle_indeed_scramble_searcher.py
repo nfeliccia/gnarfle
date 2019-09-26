4e42852aab9854a0c2270b7fb1d4f963feaa23b1
Nic Feliccia <nic@secretsmokestack.com>
2019-09-26 17:33:16 -0400

    modularisation_2 complete.

--- gnarfle_indeed_scramble_searcher.py
+++ gnarfle_indeed_scramble_searcher.py
@@ -1,80 +1,17 @@
-import re
-from collections import namedtuple
 from itertools import permutations
 from random import randrange
-
+from collections import namedtuple
 import pandas as pd
-from sqlalchemy import Integer, String, Date, Float, Column
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-from aws_login_credentials import awlc
 
 
-def create_pg_login_string():
-    """
-    This function takes no input, reads the login file, parses the information and
-    concatenates it into a string for the postress engine to use.
-    :return: string
-    """
-    # The login credentials tuple contains db_string, username, password, working_db
-    # the ask ok flag bypasses the asking if the login credentials are OK becuase
-    # continually entering y in development is a pain in the butt.
-    login_file = 'login_info.json'
-    login_credentials_tuple = awlc(login_file, askok=False)
-    aws_db_url = login_credentials_tuple[0]
-    user_id = login_credentials_tuple[1]
-    password = login_credentials_tuple[2]
-    working_db = login_credentials_tuple[3]
-    return f'postgres+psycopg2://{user_id}:{password}@{aws_db_url}:5432/{working_db}'
 
+from gnarfle_bst import start_a_sql_alchemy_session
+from gnarfle_bst import super_clean_a_string
+from gnarfle_bst import SQLIndeedSearchResults
 
-def start_a_sql_alchemy_session():
-    """ This starts  a session using SQL Alchemy tools
-    """
-    # initiate the database_session
-    db_string = create_pg_login_string()
-    db_six_cyl_engine = create_engine(db_string, echo=False)
-    database_session = sessionmaker()
-    return database_session(bind=db_six_cyl_engine)
-
-
-def super_clean_a_string(in_string: str):
-    out_string = re.sub(r'[,.;@%#?!&$()^*/\|’\':·\-]+\ *', " ", in_string).rstrip().lstrip().casefold()
-    return out_string
-
-
-Base = declarative_base()
-
-
-class SQLIndeedSearchResults(Base):
-    __tablename__ = 'indeed_search_results'
-    isr_pk = Column(Integer, primary_key=True)
-    company = Column(String)
-    extracted_url = Column(String)
-    guid = Column(String)
-    job_title_row = Column(String)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    publish_date = Column(Date)
-    job_text_raw = Column(String)
-    job_title = Column(String)
-
-    def populate(self, in_result, in_analysis):
-        self.company = in_result.company
-        self.extracted_url = in_result.extracted_url
-        self.guid = in_result.guid
-        self.job_title_row = in_result.job_title_row
-        self.latitude = in_result.latitude
-        self.longitude = in_result.longitude
-        self.publish_date = in_result.publish_date
-        self.job_text_raw = in_analysis.job_text_raw
-        self.job_title = in_analysis.job_title
 
 
 session_with_remulak = start_a_sql_alchemy_session()
-
 scramble_list_words = []
 scramble_results_tuple_list = []
 scramble_phrase_list = []
@@ -112,7 +49,8 @@ for scramble_phrase in scramble_phrase_set:
     scramble_results_tuple_list.append(ThisResult)
 
 scramble_df = pd.DataFrame(scramble_results_tuple_list)
-scramble_df.sort_values(by=['ocurances_in_title', 'ocurances_in_text','scramble_phrase'], ascending=False, inplace=True)
+scramble_df.sort_values(by=['ocurances_in_title', 'ocurances_in_text', 'scramble_phrase'], ascending=False,
+                        inplace=True)
 scramble_df.drop_duplicates(inplace=True)
 print('DataFrame', scramble_df)
 comparison_filename = f'permutations_compared-{randrange(1, 100)}.xlsx'
